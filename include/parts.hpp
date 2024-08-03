9b4312b9d3c8d7ed6f608b6079e5351fd37dc9f0
shigeno-y <37774973+shigeno-y@users.noreply.github.com>
2024-08-03 20:55:58 +0900

    next

---
+++ include/parts.hpp
@@ -0,0 +1,44 @@
+#ifndef SHIGENO_Y_VMOUSE_PARTS_INCLUDED
+#define SHIGENO_Y_VMOUSE_PARTS_INCLUDED
+
+#include "DigiMouse.h"
+
+namespace shigenoy::vmouse
+{
+    constexpr unsigned int mode = 1; // 1 or 0: LED is connected pin0 or pin1 depend on model
+    constexpr unsigned char MOUSE_DELTA = 1;
+    constexpr unsigned int SLEEP_TIME_MS = 5; // ms
+
+    void LEDon()
+    {
+        digitalWrite(mode, HIGH);
+    }
+
+    void LEDoff()
+    {
+        digitalWrite(mode, LOW);
+    }
+
+    void mouse_move(const char deltaX, const int count, const int delay)
+    {
+        for (int i = 0; i < count; ++i)
+        {
+            DigiMouse.moveX(deltaX);
+            DigiMouse.delay(delay);
+        }
+    }
+    void prepare_supply(const int delay)
+    {
+        DigiMouse.rightClick();
+        DigiMouse.delay(delay);
+    }
+
+    void deploy_supply(const int delay)
+    {
+
+        DigiMouse.leftClick();
+        DigiMouse.delay(delay);
+    }
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -9,47 +9,14 @@
 // setting: set "mode" 0 or 1
 // see http://digistump.com/wiki/digispark/tutorials/modelbi2c
 
-#include <DigiMouse.h>
+#include "parts.hpp"
 
-constexpr unsigned int mode = 1; // 1 or 0: LED is connected pin0 or pin1 depend on model
-constexpr unsigned char MOUSE_DELTA = 1;
-constexpr unsigned int SLEEP_TIME_MS = 5; // ms
+using namespace shigenoy::vmouse;
 
 void setup()
 {
   pinMode(mode, OUTPUT);
   DigiMouse.begin();
-}
-
-void LEDon()
-{
-  digitalWrite(mode, HIGH);
-}
-
-void LEDoff()
-{
-  digitalWrite(mode, LOW);
-}
-
-void mouse_move(const char deltaX, const int count, const int delay)
-{
-  for (int i = 0; i < count; ++i)
-  {
-    DigiMouse.moveX(deltaX);
-    DigiMouse.delay(delay);
-  }
-}
-void prepare_supply(const int delay)
-{
-  DigiMouse.rightClick();
-  DigiMouse.delay(delay);
-}
-
-void deploy_supply(const int delay)
-{
-
-  DigiMouse.leftClick();
-  DigiMouse.delay(delay);
 }
 
 void loop()
